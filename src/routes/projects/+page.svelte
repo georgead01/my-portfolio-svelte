767947d6c6e220f3b326a4d98b6312199c0a3889
George Abu Daoud <abudauodg@gmail.com>
2024-03-06 14:49:54 -0500

    add projects loop

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -8,4 +8,10 @@
 
 <h1>projects</h1>
 
-<pre>{ JSON.stringify(projects, null, "\t") }</pre>
+{#each projects as p}
+	<article>
+		<h2>Lorem ipsum dolor sit.</h2>
+		<img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dolor quos, quod assumenda explicabo odio, nobis ipsa laudantium quas eum veritatis ullam sint porro minima modi molestias doloribus cumque odit.</p>
+	</article>
+{/each}
