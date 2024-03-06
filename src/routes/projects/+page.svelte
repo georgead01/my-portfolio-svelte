7bebb3538c4fffd52b3b34cd4a20824df82f84ce
George Abu Daoud <abudauodg@gmail.com>
2024-03-06 14:44:44 -0500

    projects json and svelte

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,72 +1,10 @@
 <svelte:head>
+    <script>
+        import projects from '$lib/projects.json';
+    </script>
 	<title>projects</title>
 </svelte:head>
 
 <h1>projects</h1>
 
-<div class="projects">
-    <a href="assignments/A2/index.html" class="project_link">
-    <article>
-        <h2>exploratory single-family zoning data visualization</h2>
-        <img src="housing.jpeg" alt="">
-        <p>"Urban planners have increasingly demonstrated how the 'yard and picket fence' style of US housing has negative effects on housing affordability, equity, and transportation, yet single-family zoning persists. Which communities in the Boston area have the most exclusionary zoning? Where are there opportunities for change?" -course website</p>
-    </article>
-    </a>
-    <a href="assignments/A3/index.html" class="project_link">
-    <article>
-        <h2>how did the population composition in america change over the 20th century</h2>
-        <img src="main_pic.png" alt="">
-        <p>Quod magni, impedit voluptas nemo animi possimus reiciendis ex repellendus ad accusamus beatae illum, ipsam voluptate dolorum, ab in dicta fuga. Eum deserunt perspiciatis harum molestiae? Voluptate perferendis iure corporis?</p>
-    </article>
-    </a>
-    <article>
-        <h2>Dolor deserunt nemo saepe!</h2>
-        <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
-        <p>Labore iure impedit itaque hic illum omnis ut, possimus minima odio quis magni eum assumenda earum ea magnam nesciunt in pariatur? Quis nisi autem eligendi sunt totam tempore iure harum!</p>
-    </article>
-    <article>
-        <h2>Mollitia expedita officia culpa.</h2>
-        <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
-        <p>Unde, aut, quos corporis adipisci, provident voluptatem dolore nobis at eius atque magnam earum esse reprehenderit optio. Autem unde dolore voluptas a blanditiis, consequuntur numquam aperiam eius, itaque, perferendis praesentium?</p>
-    </article>
-    <article>
-        <h2>Nihil quis harum vel?</h2>
-        <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
-        <p>Dicta incidunt tenetur temporibus, odio assumenda natus adipisci explicabo quos architecto accusantium eveniet odit facere asperiores consectetur laboriosam hic inventore dolorem repellat minus quis, tempora eos et reiciendis ipsa. Explicabo.</p>
-    </article>
-    <article>
-        <h2>Explicabo facilis culpa corrupti?</h2>
-        <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
-        <p>Eius, mollitia laudantium cum placeat dolorum, optio nobis, aut maiores illo sit voluptatum eos saepe commodi qui! Modi, consequatur. Doloribus aspernatur aut eveniet minima quibusdam laudantium quaerat tempore voluptates rerum?</p>
-    </article>
-    <article>
-        <h2>Adipisci, error. Fugit, in!</h2>
-        <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
-        <p>Vitae expedita ex sapiente ducimus laboriosam vero facilis deleniti ullam tenetur delectus inventore, tempora nobis quo voluptate? Unde delectus quidem ratione, quam fugit eos aliquid alias eveniet, similique necessitatibus voluptatum?</p>
-    </article>
-    <article>
-        <h2>Ratione alias aperiam amet?</h2>
-        <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
-        <p>Saepe delectus ipsam quas consectetur tempora. Ipsum in perferendis similique ab explicabo? Quibusdam recusandae animi explicabo, laudantium repellendus magni eum molestiae ducimus eveniet ratione vel sapiente perferendis facere dolore similique?</p>
-    </article>
-    <article>
-        <h2>Dicta suscipit nesciunt pariatur.</h2>
-        <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
-        <p>Quia obcaecati repellendus sapiente exercitationem nobis nihil quibusdam veniam explicabo, quam nostrum, magnam qui molestiae neque, veritatis modi! Quo aspernatur harum doloribus deleniti! Rem expedita animi adipisci. Harum, quia autem!</p>
-    </article>
-    <article>
-        <h2>Dignissimos veritatis recusandae sunt.</h2>
-        <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
-        <p>Natus, numquam quod eum, sunt quidem odio delectus culpa rerum voluptatibus facilis maiores explicabo consequuntur. Neque impedit eos at quas veniam officiis voluptates corrupti id incidunt, deserunt modi eaque corporis.</p>
-    </article>
-    <article>
-        <h2>Error repellendus officia blanditiis?</h2>
-        <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
-        <p>Blanditiis magnam autem ratione, porro cumque incidunt officia in dolorem voluptatibus adipisci, quibusdam nisi. Temporibus fugit est vel alias, eligendi nemo facilis modi placeat voluptatibus quaerat tenetur corrupti eveniet eos.</p>
-    </article>
-    <article>
-        <h2>Quidem dolor incidunt repudiandae?</h2>
-        <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
-        <p>Temporibus necessitatibus dicta aliquam quae delectus, provident explicabo corrupti, voluptas eum nobis accusamus, dolorum sunt hic aliquid! Beatae quasi recusandae, nisi ducimus delectus ea exercitationem iste harum debitis adipisci nostrum?</p>
-    </article>
-</div>
+<pre>{ JSON.stringify(projects, null, "\t") }</pre>
